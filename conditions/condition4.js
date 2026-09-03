cebf1d96ea36b798488622d8b4819ee1e42a5dff
youssef boukacha <youssefboukacha37@gmail.com>
2026-09-03 18:29:50 +0200

    conditions

--- conditions/condition4.js
+++ conditions/condition4.js
@@ -2,6 +2,6 @@ var prompt = require('prompt-sync')();
 let annes = parseInt(prompt("entrez annes "));
 console.log(annes * 12);
 console.log(annes * 365);
-console.log(annes * 8 760);
-console.log(annes * 525 600);
-console.log(annes * 31 536 000)
+console.log(annes * 8760);
+console.log(annes * 525600);
+console.log(annes * 31536000);
